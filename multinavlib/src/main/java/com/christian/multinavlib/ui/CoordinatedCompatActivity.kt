55cd559bc109251ea1c61bca4f82f1da9d477613
Christian B <christian.bergmann@kunstadt.de>
2019-07-15 09:17:48 +0200

    Add base compat activity

--- multinavlib/src/main/java/com/christian/multinavlib/ui/CoordinatedCompatActivity.kt
+++ multinavlib/src/main/java/com/christian/multinavlib/ui/CoordinatedCompatActivity.kt
@@ -1,9 +1,10 @@
 package com.christian.multinavlib.ui
 
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentActivity
 import com.christian.multinavlib.navigation.coordinator.CoordinatorManager
 import org.koin.android.ext.android.inject
 
-abstract class CoordinatedCompatActivity: FragmentActivity() {
+abstract class CoordinatedCompatActivity: AppCompatActivity() {
     protected val coordinatorManager: CoordinatorManager by inject()
 }
